bc45bd1677ed32bab12104d24569ef0620c19038
MhlvDenis <dns.aqua.ban@gmail.com>
2022-03-09 21:20:45 +0300

    Fix PositionalAppearance

--- core/visual/PositionalAppearance.hpp
+++ core/visual/PositionalAppearance.hpp
@@ -2,18 +2,23 @@
 #define MAD_POSITIONALAPPEARANCE_HPP
 
 #include <event/VisualEvent.hpp>
-#include <visual/image/Image.hpp>
-#include <common/FVec2D.hpp>
 
 #include <memory>
 
 
+// Forward declarations
+namespace mad::core {
+    class Vec2d;
+    class Image;
+}
+
 namespace mad::core {
 
     class PositionalAppearance : public VisualEvent {
-        std::unique_ptr<Vec2d> position;
-        std::shared_ptr<Vec2d> image;
-        int z_index;
+    private:
+        std::unique_ptr<Vec2d> m_position;
+        std::shared_ptr<Image> m_image;
+        int m_z_index;
     };
 
 }
