65e89df949e29c6cccdc17724a2bc37b83651f3a
MhlvDenis <dns.aqua.ban@gmail.com>
2022-03-10 10:58:36 +0300

    Add constructor PositionalAppearance

--- core/visual/PositionalAppearance.hpp
+++ core/visual/PositionalAppearance.hpp
@@ -1,26 +1,25 @@
 #ifndef MAD_POSITIONALAPPEARANCE_HPP
 #define MAD_POSITIONALAPPEARANCE_HPP
 
+#include <common/FVec2D.hpp>
 #include <event/VisualEvent.hpp>
+#include <visual/image/Image.hpp>
 
 #include <memory>
 
 
-// Forward declarations
-namespace mad::core {
-    class Vec2d;
-    class Image;
-}
-
 namespace mad::core {
 
     class PositionalAppearance : public VisualEvent {
+    public:
+        PositionalAppearance(std::unique_ptr<Vec2d> pos, std::shared_ptr<Image> img, int z_index);
+
     private:
         std::unique_ptr<Vec2d> m_position;
         std::shared_ptr<Image> m_image;
         int m_z_index;
     };
 
-}
+}// namespace mad::core
 
-#endif //MAD_POSITIONALAPPEARANCE_HPP
+#endif//MAD_POSITIONALAPPEARANCE_HPP
