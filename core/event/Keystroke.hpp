eacc58e899387ddd5e20cc5a9dde5f10a16699c8
MhlvDenis <dns.aqua.ban@gmail.com>
2022-03-11 18:15:19 +0300

    Commit

---
+++ core/event/Keystroke.hpp
@@ -0,0 +1,14 @@
+#ifndef MAD_KEYSTROKE_H
+#define MAD_KEYSTROKE_H
+
+#include <event/Event.hpp>
+
+namespace mad::core {
+
+    class Keystroke : public Event {
+
+    };
+
+}
+
+#endif //MAD_KEYSTROKE_H
