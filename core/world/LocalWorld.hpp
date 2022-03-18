424196c5470f5d2c12a3e6e7b2b31d0abcd962ec
Мирон <ghanin03@list.ru>
2022-03-18 14:35:05 +0300

    test

--- core/world/LocalWorld.hpp
+++ core/world/LocalWorld.hpp
@@ -5,8 +5,16 @@
 #include <world/entity/EntityStorage.hpp>
 #include <event/management/dispatcher/ImmediateDispatcher.hpp>
 #include <event/management/dispatcher/DelayedDispatcher.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Window/Event.hpp>
 
 #include <queue>
+#include "box2d/box2d.h"
+#include "visual/image/shape/Shape.hpp"
 
 
 namespace mad::core {
@@ -19,12 +27,19 @@ namespace mad::core {
 
         void produce(EventDispatcher &dispatcher) override;
 
-        Entity::Id create_viewable_entity(int z_ind, Vec2d initial_position, std::shared_ptr<Image> image) override;
+        Entity::Id create_viewable_entity(int z_ind, Vec2d initial_position, float initial_rotation, std::shared_ptr<Image> image) override;
+
+        Entity::Id create_physical_entity(int z_ind, Vec2d initial_position, float initial_rotation, std::shared_ptr<Image> image, bool is_Fixed = false) override;
 
     private:
         std::shared_ptr<std::queue<std::shared_ptr<Event>>> m_step_events_queue;
         std::unique_ptr<DelayedDispatcher> m_event_queue_dispatcher;
         EntityStorage m_storage;
+        b2World physicalWorld;
+        float dt;
+        float render_scale = 5;
+        sf::Clock clock;
+        float lastTime = 0;
     };
 
 }
