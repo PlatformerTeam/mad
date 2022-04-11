c1be8d834201cd8baf494e7d90e54fcabcbea255
MhlvDenis <dns.aqua.ban@gmail.com>
2022-04-11 12:20:54 +0300

    Fix targets

--- game/game_runner_example.cpp
+++ game/game_runner_example.cpp
@@ -1,6 +1,70 @@
-#include <iostream>
-using namespace std;
+#include <event/management/producer/EventProducer.hpp>
+#include <event/management/dispatcher/EventDispatcher.hpp>
+#include <event/management/handler/WindowCloseHandler.hpp>
+#include <event/management/producer/SystemListener.hpp>
+#include <event/system/KeyHeld.hpp>
+#include <event/system/KeyPressed.hpp>
+#include <runner/SequentialRunner.hpp>
+#include <visual/Camera.hpp>
+#include <world/LocalWorld.hpp>
+#include <world/entity/ViewableEntity.hpp>
+
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <spdlog/spdlog.h>
+
+
+class ArrowController : public mad::core::EventHandler {
+public:
+    explicit ArrowController(std::shared_ptr<mad::core::World> world,
+                             mad::core::Entity::Id entity_id)
+        : m_world(std::move(world)),
+          m_entity_id(entity_id) {}
+
+    void handle(const mad::core::Event &event) override {
+        SPDLOG_INFO("handle arrow event");
+
+        auto make_move_intent = [](mad::core::Vec2d dir) {
+            return mad::core::LambdaIntent(
+                    [=](mad::core::Entity &entity, mad::core::EventDispatcher &event_dispatcher) {
+                        mad::core::cast_to<mad::core::ViewableEntity>(entity).move(dir);
+                    });
+        };
+
+        if (event.type == mad::core::Event::Type::KeyPressed) {
+            const auto &keystroke = mad::core::const_cast_to<mad::core::KeyPressed>(event);
+            if (keystroke.key_id == sf::Keyboard::Key::Up) {
+                m_world->manipulate_entity_id(m_entity_id, make_move_intent(mad::core::Vec2d{0.0f, -10.0f}));
+            } else if (keystroke.key_id == sf::Keyboard::Key::Down) {
+                m_world->manipulate_entity_id(m_entity_id, make_move_intent(mad::core::Vec2d{0.0f, 10.0f}));
+            } else if (keystroke.key_id == sf::Keyboard::Key::Left) {
+                m_world->manipulate_entity_id(m_entity_id, make_move_intent(mad::core::Vec2d{-10.0f, 0.0f}));
+            } else if (keystroke.key_id == sf::Keyboard::Key::Right) {
+                m_world->manipulate_entity_id(m_entity_id, make_move_intent(mad::core::Vec2d{10.0f, 0.0f}));
+            }
+        } else if (event.type == mad::core::Event::Type::KeyHeld) {
+            const auto &keystroke = mad::core::const_cast_to<mad::core::KeyHeld>(event);
+            if (keystroke.key_id == sf::Keyboard::Key::Up) {
+                m_world->manipulate_entity_id(m_entity_id, make_move_intent(mad::core::Vec2d{0.0f, -0.01f}));
+            } else if (keystroke.key_id == sf::Keyboard::Key::Down) {
+                m_world->manipulate_entity_id(m_entity_id, make_move_intent(mad::core::Vec2d{0.0f, 0.01f}));
+            } else if (keystroke.key_id == sf::Keyboard::Key::Left) {
+                m_world->manipulate_entity_id(m_entity_id, make_move_intent(mad::core::Vec2d{-0.01f, 0.0f}));
+            } else if (keystroke.key_id == sf::Keyboard::Key::Right) {
+                m_world->manipulate_entity_id(m_entity_id, make_move_intent(mad::core::Vec2d{0.01f, 0.0f}));
+            }
+        }
+    }
+
+    std::unordered_set<mad::core::Event::Type> handled_types() override {
+        return {mad::core::Event::Type::KeyPressed, mad::core::Event::Type::KeyHeld};
+    }
+
+private:
+    std::shared_ptr<mad::core::World> m_world;
+    mad::core::Entity::Id m_entity_id;
+};
 
 int main() {
-    cout << "Hello from game-runner-example" << '\n';
+
 }
