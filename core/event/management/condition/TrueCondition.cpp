4c72a61be7f23d0f0cfa87b39f8ebe0debdcc608
Мирон <ghanin03@list.ru>
2022-05-12 11:47:00 +0300

    create statemachine

--- core/event/management/condition/TrueCondition.cpp
+++ core/event/management/condition/TrueCondition.cpp
@@ -4,5 +4,5 @@ bool mad::core::TrueCondition::is_triggered_by(const mad::core::Event &event) {
     return true;
 }
 std::unordered_set<mad::core::Event::Type> mad::core::TrueCondition::triggers() {
-    return {mad::core::Event::Type::KeyPressed};
+    return {mad::core::Event::Type::KeyPressed, mad::core::Event::Type::Collision, mad::core::Event::Type::KeyHeld, mad::core::Event::Type::KeyReleased, mad::core::Event::Type::LevelPause, mad::core::Event::Type::Menu, mad::core::Event::Type::Movement, mad::core::Event::Type::Runner, mad::core::Event::Type::Visual, mad::core::Event::Type::WindowClose};
 }
--- core/event/management/controller/statemachine/condition/TrueCondition.cpp
+++
@@ -1,8 +0,0 @@
-#include "TrueCondition.hpp"
-
-bool mad::core::TrueCondition::is_triggered_by(const mad::core::Event &event) {
-    return true;
-}
-std::unordered_set<mad::core::Event::Type> mad::core::TrueCondition::triggers() {
-    return {mad::core::Event::Type::All};
-}
