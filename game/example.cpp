6f5a8298632a95493862f280f304bef8c84b7be6
alexeyha <piratlex2003@mail.ru>
2022-05-13 21:27:31 +0300

    Erase part of example

--- game/example.cpp
+++ game/example.cpp
@@ -152,17 +152,17 @@ int main(int argc, char *argv[]) {
             std::make_shared<mad::core::StaticImage>(mad::core::StaticImage("../../game/resources/static/brick.png", 300.0f, 50.0f, mad::core::StaticImage::TransformType::Fit))
     );
 
-    mad::core::Entity::Id animated_id_1 = world->create_viewable_entity(
-            5,
-            mad::core::Vec2d{180.0f, 240.0f},0,
-            std::make_shared<mad::core::AnimatedImageOneFile>(mad::core::AnimatedImageOneFile("../../game/resources/animated/runner_new.png", 5, 2, 120, 1400, 770))
-    );
-
-    mad::core::Entity::Id animated_id_2 = world->create_viewable_entity(
-            6,
-            mad::core::Vec2d{380.0f, 70.0f},0,
-            std::make_shared<mad::core::AnimatedImageOneFile>(mad::core::AnimatedImageOneFile("../../game/resources/animated/helicopter.png", 1, 4, 150, 423, 600))
-    );
+//    mad::core::Entity::Id animated_id_1 = world->create_viewable_entity(
+//            5,
+//            mad::core::Vec2d{180.0f, 240.0f},0,
+//            std::make_shared<mad::core::AnimatedImageOneFile>(mad::core::AnimatedImageOneFile("../../game/resources/animated/runner_new.png", 5, 2, 120, 1400, 770))
+//    );
+//
+//    mad::core::Entity::Id animated_id_2 = world->create_viewable_entity(
+//            6,
+//            mad::core::Vec2d{380.0f, 70.0f},0,
+//            std::make_shared<mad::core::AnimatedImageOneFile>(mad::core::AnimatedImageOneFile("../../game/resources/animated/helicopter.png", 1, 4, 150, 423, 600))
+//    );
 
     /*mad::core::StaticImage st_im_2 = mad::core::StaticImage("../../game/resources/static/18plus.png");
     st_im_2.set_shape(400, 400, mad::core::StaticImage::TransformType::Tile);
