5e9695828d16ff5f44dba36ef7735b36594bad55
Мирон <ghanin03@list.ru>
2022-05-14 22:04:45 +0300

    set animations in statemachine

--- core/event/management/controller/Idle.cpp
+++ core/event/management/controller/Idle.cpp
@@ -1,6 +1,8 @@
 #include "Idle.hpp"
-mad::core::Idle::Idle() {
+mad::core::Idle::Idle(std::shared_ptr<mad::core::LocalWorld> world, mad::core::Entity::Id entity_id) : m_world(world), m_entity_id(entity_id){
+    m_entity = cast_to_or_null<PhysicalEntity>(m_world->get_storage().get_entity(m_entity_id));
 }
-void mad::core::Idle::control() {
 
+void mad::core::Idle::control() {
+    m_entity->set_action(ImageStorage::TypeAction::Idle);
 }
