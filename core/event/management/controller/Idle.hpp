773fbdaed9aea4d114cf69a85322de00f598d38f
Мирон <ghanin03@list.ru>
2022-05-14 22:04:45 +0300

    set animations in statemachine

--- core/event/management/controller/Idle.hpp
+++ core/event/management/controller/Idle.hpp
@@ -1,17 +1,20 @@
 #ifndef MAD_IDLE_HPP
 #define MAD_IDLE_HPP
 #include "Controller.hpp"
+#include "world/LocalWorld.hpp"
 
 namespace mad::core {
 
     class Idle : public Controller {
     public:
-        explicit Idle();
+        explicit Idle(std::shared_ptr<mad::core::LocalWorld> world, Entity::Id entity_id);
 
         void control() override;
 
     private:
-
+        std::shared_ptr<mad::core::LocalWorld> m_world;
+        Entity::Id m_entity_id;
+        PhysicalEntity* m_entity;
     };
 
 }
