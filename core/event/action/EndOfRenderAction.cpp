5662bad52152237cf5f34707e09ea2e13107a3fc
alexeyha <piratlex2003@mail.ru>
2022-05-15 17:29:09 +0300

    Add event

---
+++ core/event/action/EndOfRenderAction.cpp
@@ -0,0 +1,4 @@
+#include "EndOfRenderAction.hpp"
+
+
+
---
+++ core/event/action/EndOfRenderAction.hpp
@@ -0,0 +1,17 @@
+#ifndef MAD_ENDOFRENDERACTION_HPP
+#define MAD_ENDOFRENDERACTION_HPP
+
+#include <event/Event.hpp>
+
+namespace mad::core {
+
+    class EndOfRenderAction : public Event {
+    public:
+
+
+
+    };
+
+}
+
+#endif //MAD_ENDOFRENDERACTION_HPP
