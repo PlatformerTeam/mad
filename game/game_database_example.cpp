84d2adf2166f92b973c216ac9e4919a859bb1996
denis <dns.aqua.ban@gmail.com>
2022-05-28 16:05:17 +0300

    Add httplib.h in submodule, move resources, add network directory and cmake

--- game/game_database_example.cpp
+++ game/game_database_example.cpp
@@ -45,8 +45,8 @@ int main() {
     auto database_storage_driver = std::make_shared<mad::core::DatabaseClientStorageDriver>(database);
 
     std::vector<std::shared_ptr<mad::core::LevelLoader>> level_loaders{
-            std::make_shared<mad::core::LevelLoaderFromFile>("../../game/resources/levels/level_with_finish"),
-            std::make_shared<mad::core::LevelLoaderFromFile>("../../game/resources/levels/level_01")
+            std::make_shared<mad::core::LevelLoaderFromFile>("../../resources/levels/level_with_finish"),
+            std::make_shared<mad::core::LevelLoaderFromFile>("../../resources/levels/level_01")
     };
 
     auto game_runner = std::make_unique<mad::core::GameRunner>(
